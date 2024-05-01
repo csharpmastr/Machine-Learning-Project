e87317abd6c184253f22366d0d74aadd733e2417
rhyliieee <csharpmastr@gmail.com>
2024-05-01 12:46:12 +0800

    Implemented Data Ingestion

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## Labnostic End to End Machine Learning Project
+First Install Required packages in requirements.txt file
+Perform data ingestion by running the code from data_ingestion.py
---
+++ src/components/data_ingestion.py
@@ -0,0 +1,59 @@
+import os
+import sys
+
+from optree import CustomTreeNode
+from src.exception import CustomException
+from src.logger import logging
+
+import pandas as pd
+from dataclasses import dataclass
+from sklearn.model_selection import train_test_split
+
+# direct exported data to a specific file path
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('artifacts', 'train_data.csv')
+    test_data_path: str=os.path.join('artifacts', 'test_data.csv')
+    raw_data_path: str=os.path.join('artifacts', 'raw_data.csv')
+
+
+# import data from sources
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+        
+    # perform data retrieval
+    def initiate_data_ingestion(self):
+        logging.info('Started Data Ingestion Method')
+        
+        try:
+            data = pd.read_csv('notebook\data\diabetes1.csv')
+            logging.info('Imported .csv Data as a Pandas DataFrame')
+            
+            # create artifacts directory (if existing, just replace the folder)
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            
+            # save raw pandas data to csv
+            data.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            
+            logging.info('Train test split Initiated')
+            train_data, test_data = train_test_split(data, test_size=0.2, random_state=62)
+            
+            # save train and test data to csv
+            train_data.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_data.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            
+            logging.info('Data Ingestion Completed')
+            
+            # return train and test data path for data transformation purposes
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e, sys)
+
+if __name__=='__main__':
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
